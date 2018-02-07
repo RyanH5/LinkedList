9ba7df9076903af29cee59957bbde89c14e6ee49
Sage Vogt <thurmanvogt@gmail.com>
2018-02-06 17:43:57 -0700

    Add total and read card count in JS

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ $('#section-bookmark-list').on('click', '.bookmark__btn-read',toggleRead);
 $('#section-bookmark-list').on('click', '.bookmark__btn-delete', removeBookmark);
 $('#form__input-title').on('keyup', requireTitleAndURL);
 $('#form__input-url').on('keyup', requireTitleAndURL);
+$('#form__btn-submit').on('click', updateCount)
+$('#section-bookmark-list').on('click', '.bookmark__btn-read', updateCount)
+$('#section-bookmark-list').on('click', '.bookmark__btn-delete', updateCount)
+
+function updateCount(){
+  var counterTotal = $('.bookmark__btn-delete').length;
+  var counterRead = $('.read').length;
+  $('.counter__total').text(counterTotal);
+  $('.counter__read').text(counterRead);
+}  
 
 function clearAndFocus() {
   $('#form__input-title').val('');
