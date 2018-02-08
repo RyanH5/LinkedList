31ddf0ce701fa923c7dad48e1aa73148e25a2310
Sage Vogt <thurmanvogt@gmail.com>
2018-02-08 09:16:31 -0700

    final

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $('#form__btn-submit').on('click', prependBookmark);
 $('#section-bookmark-list').on('click', '.bookmark__btn-read',toggleRead);
 $('#section-bookmark-list').on('click', '.bookmark__btn-delete', removeBookmark);
-$('#bookmark-remove-all-read').on('click', removeAllRead);
-$('#form__input-title').on('keyup', requireTitleAndURL);
-$('#form__input-url').on('keyup', requireTitleAndURL);
+$('#bookmark-remove-all-read').on('dblclick', removeAllRead);
 $('#form__btn-submit').on('click', updateCount);
 $('#section-bookmark-list').on('click', '.bookmark__btn-read', updateCount);
 $('#section-bookmark-list').on('click', '.bookmark__btn-delete', updateCount);
-$('#form__input-url').on('keyup', isValidURL);
+$('#form__input-title').on('input', isValidURLTitle);
+$('#form__input-url').on('input', isValidURLTitle);
+$('div').mouseover(feedbackPopup);
 
 function updateCount(){
   var counterTotal = $('.bookmark__btn-delete').length;
@@ -16,6 +16,7 @@ function updateCount(){
   $('#bookmark-count-read').text('Bookmarks read: ' + counterRead);
 }  
 
+
 function clearAndFocus() {
   $('#form__input-title').val('');
   $('#form__input-url').val('');
@@ -23,34 +24,25 @@ function clearAndFocus() {
   $('#form__input-title').focus();
 }
 
-function requireTitleAndURL() {
-  if ($('#form__input-title').val() !== '' && $('#form__input-url').val() !== '') {
+function  isValidURLTitle() {
+  var myVariable = $('#form__input-url').val();
+  if($('#form__input-title').val() !== '' && /^(www\.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(myVariable)) {
     $('#form__btn-submit').attr('disabled', false);
   } 
 }
 
-function  isValidURL() {
-  var myVariable = $('#form__input-url').val();
-if(/^(http:\/\/www\.|https:\/\/www\.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(myVariable)){
-    $('#form__btn-submit').attr('disabled', false);;
-} else {
-    $('#form__btn-submit').attr('disabled', true);;
-}
-}
-
 function prependBookmark(e) {
-  requireTitleAndURL();
-    e.preventDefault();
-    $('#section-bookmark-list').prepend(`
-    <article class="bookmark">
-      <h2 class="bookmark__website-title">${$('#form__input-title').val()}</h2>
-      <hr>
-      <a href="" class="bookmark__website-link">${$('#form__input-url').val()}</a>
-      <hr>
-      <button class="bookmark__btn-read">Read</button>
-      <button class="bookmark__btn-delete">Delete</button>
-    </article>`);
-    clearAndFocus();
+  e.preventDefault();
+  $('#section-bookmark-list').prepend(`
+  <article class="bookmark">
+    <h2 class="bookmark__website-title">${$('#form__input-title').val()}</h2>
+    <hr>
+    <a href="" class="bookmark__website-link">${$('#form__input-url').val()}</a>
+    <hr>
+    <button class="bookmark__btn-read">Read</button>
+    <button class="bookmark__btn-delete">Delete</button>
+  </article>`);
+  clearAndFocus();
 }
 
 function toggleRead() {
@@ -67,6 +59,21 @@ function removeAllRead() {
   bookmarksRead.parent().remove();
   updateCount();
 }
+
+function feedbackPopup() {
+  var modal = $('#myModal');
+  var span = $(".close");
+  var myVariable = $('#form__input-url').val();
+    if(myVariable === '' || /^(www\.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(myVariable)) {
+      modal.css("display", "none");
+    } else {
+      modal.css("display", "block");
+    }
+    span.on('click', function() {
+        modal.css("display", "none");
+    })
+}
+
 
 
 
